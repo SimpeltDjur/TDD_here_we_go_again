aab51d8a31ee0da1d0a98add63dea932a9854f83
SimpeltDjur <SimpeltDjur@gmail.com>
2022-09-21 09:22:55 +0200

    kod tillfixad för första testet

--- src/main/java/UserGrej.java
+++ src/main/java/UserGrej.java
@@ -1,4 +1,6 @@
+import java.util.Base64;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class UserGrej {
 
@@ -7,12 +9,25 @@ public class UserGrej {
         this.someDB = someDB;
     }
 
-    public boolean loggIn(String userName, String passWord) {
+    public String loggIn(String userName, String passWord) {
 
         HashMap<String, String> userMap = someDB.getUsers();
 
+        String orginal = userName;
+        byte[] orginalAsBytes = orginal.getBytes();
+        byte[] orginalAsBase64 = Base64.getEncoder().encode(orginalAsBytes);
+        String byte64String = new String(orginalAsBase64);
 
 
-        return userMap.containsKey(userName) ? userMap.get(userName) == passWord : false;
+
+        boolean legit = userMap.containsKey(userName) && Objects.equals(userMap.get(userName), passWord);
+
+        if (legit){
+            return byte64String;
+        }
+        else {
+            return "Bajs";
+        }
+
     }
 }
