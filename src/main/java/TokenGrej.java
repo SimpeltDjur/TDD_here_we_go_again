a12e553a621877fe99f203c3ae4691169a9512f0
SimpeltDjur <SimpeltDjur@gmail.com>
2022-09-21 15:02:55 +0200

    Snyggade till min token så att den blir som förväntad token.

--- src/main/java/TokenGrej.java
+++ src/main/java/TokenGrej.java
@@ -1,12 +1,16 @@
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
+import java.awt.font.TextHitInfo;
 import java.security.Key;
 import java.util.Map;
 
 public class TokenGrej {
-    public TokenGrej(SomeDB someDB) {
 
+    private SomeDB someDB;
+
+    public TokenGrej(SomeDB someDB) {
+        this.someDB = someDB;
     }
 
     public String getToken(String username) {
@@ -15,7 +19,7 @@ public class TokenGrej {
 
         String token = Jwts.builder()
                 .setSubject(username)
-                .addClaims(Map.of("Lastname", "Gunnarsson"))
+                .addClaims(Map.of("Roll", someDB.getRoll()))
                 .signWith(key)
                 .compact();
 
